83ba5550714700b7b329eb613f53723da2388404
ozneroL541 <l.21radice@gmail.com>
2023-07-12 18:31:20 +0200

    try catch

--- src/Algorithms/Examples/Max.java
+++ src/Algorithms/Examples/Max.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 
 public class Max {
     public void calcMax() {
-        Scanner sc = new Scanner(System.in);
-        int [] s = new int[3];
+        try (Scanner sc = new Scanner(System.in)) {
+            int [] s = new int[3];
 
-        s[1] = sc.nextInt();
-        while ( s[1] != 0 ) {
-            if ( s[1] != 0 ) {
-                s[0] = s[1];
-                s[2] = sc.nextInt();
-                s[0] = s[0] - s[2];
+            s[1] = sc.nextInt();
+            while ( s[1] != 0 ) {
+                if ( s[1] != 0 ) {
+                    s[0] = s[1];
+                    s[2] = sc.nextInt();
+                    s[0] = s[0] - s[2];
+                }
+                if ( ! (s[0] > 0) ){
+                    s[0] = s[2];
+                    s[1] = s[0];
+                }
             }
-            if ( ! (s[0] > 0) ){
-                s[0] = s[2];
-                s[1] = s[0];
-            }
+            System.out.println( "Max" + s[1] );
         }
-        System.out.println( "Max" + s[1] );
     }
 }
