60cf9cb117a2022e521d8a06e39e6382dbc9fc6d
ozneroL541 <l.21radice@gmail.com>
2023-07-12 21:50:29 +0200

    dei min

---
+++ src/Algorithms/Sorting/Advanced/Min.java
@@ -0,0 +1,29 @@
+package Algorithms.Sorting.Advanced;
+
+public class Min {
+    /**
+     * It calculates the minor in an array a
+     * @param a array
+     * @param lo lower bound of the array
+     * @param hi higher boung of the array
+     */
+    public static int min ( int [] a, int lo, int hi ) {
+        // If low equals high
+        if ( lo == hi )
+            // Return it
+            return a[lo];
+        // Calculate the middle of the array
+        int mid = ( lo + hi ) / 2;
+        // Calculate the minor of the first half of the array
+        int min1 = min( a, lo, mid );
+        // Calculate the minor of the second half of the sarray
+        int min2 = min( a, mid+1, hi );
+        // If the first minor is lower
+        if ( min1 < min2 )
+            // Return the minor
+            return min1;
+        else
+            // Return the minor
+            return min2;
+    }
+}
