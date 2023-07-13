054a3390133cbc08c571bfab7a7b78a618432a78
ozneroL541 <l.21radice@gmail.com>
2023-07-13 20:08:29 +0200

    partitionsort

--- src/Algorithms/Sorting/Advanced/Quick.java
+++ src/Algorithms/Sorting/Advanced/Quick.java
@@ -3,21 +3,40 @@ package Algorithms.Sorting.Advanced;
 import Algorithms.Sorting.Sort;
 
 public class Quick extends Sort {
+    /**
+     * The array is divided in elements bigger
+     * and smaller than the pivot.
+     * @param a array
+     * @param l pivot
+     * @param r last element of the array
+     * @return pivot
+     */
     protected static int partition( Comparable[] a, int l, int r ) {
-        int i = l,j = r + 1;
+        // Set i as the beginning and j ad the end
+        int i = l, j = r + 1;
+        // Copy the array in v
         Comparable v = a[l];
+        // For ever 
         while ( true ){
+            // Increment i and go on if a[i] is less than v
             while ( less( a[++i], v ) )
+                // Stop when i reaches r
                 if( i == r )
                     break;
-        while ( less( v, a[--j] ) )
-            if( j == l )
-                break;;
-        if ( i >= j ) 
-            break;
-        exch( a, i, j );
+            // Decrease j and go on if a[j] is more than v
+            while ( less( v, a[--j] ) )
+                // Stop when j reaches l
+                if( j == l )
+                    break;
+            // Stop if i is more than j
+            if ( i >= j ) 
+                break;
+            // Exchange i with j
+            exch( a, i, j );
         }
+        // Exchange l with j
         exch( a, l, j );
+        // Return j
         return j;
     }
     public static void sort( Comparable[] a ){
