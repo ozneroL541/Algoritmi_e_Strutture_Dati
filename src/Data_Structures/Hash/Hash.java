5ad748d3c3add9e3357120716c28d89b1f40aade
ozneroL541 <l.21radice@gmail.com>
2023-07-26 11:06:31 +0200

    c

--- src/Data_Structures/Hash/Hash.java
+++ src/Data_Structures/Hash/Hash.java
@@ -1,29 +1,74 @@
 package Data_Structures.Hash;
 
 public class Hash {
-    int DIMALFA = 0;
-    int DIMTAB = 128;
-    public int Hash( String v ){
-        int h,l,m;
-        char c;
-        l = v.length();
-        m = 0;
-        for( h = 1; h <= l; h++ ){
-        c = v.charAt(h);
-        m = ( DIMALFA*m + getNumericValue(c)) % DIMTAB;
-        }
+    // Table dimension constant
+    private final int DIMTAB = 81; // A random coprime of 256
+    // Void constant
+    private final Element VOID = null;
+    // Deleted element constant
+    private final Element DELETED = new Element("","");
+    // Table
+    private Element[] Tab = new Element[DIMTAB];
+    // Size
+    private int size = 0;
+    /**
+     * Hash function
+     * Hash function calculates the position
+     * of the data in the table.
+     * @param k data
+     * @return position of the data
+     */
+    public int Hash(String k){
+        int m = 0; int c;
+        for( int h = 0; h < k.length(); h++ ){
+            c = k.charAt(h);
+            m = ( 256 * m + c ) % DIMTAB;
+        } 
         return m;
     }
-    private int getNumericValue(char c) {
-        return 0;
+    /**
+     * Insert the element in the table
+     * @param e element to insert
+     */
+    public void Insert(Element e) {
+        if ( size < DIMTAB ){
+            int i = Hash(e.key);
+            while( Tab[i] != VOID && Tab[i] != DELETED ){
+                i = ( i + 1 ) % DIMTAB;
+            }
+                Tab[i] = e;
+                size++; 
+        } else
+            System.out.println("HashTable is full");
+    } 
+    /**
+     * Delete the element from the table
+     * @param e element to delete
+     */
+    public void Delete(Element e) {
+        int i = Hash(e.key);
+        int j = 0;
+        while( j <= DIMTAB && Tab[i] != VOID && Tab[i] != e ){
+            i = (i+1)%DIMTAB;
+            j++;
+        }
+        if ( Tab[i] == e ){
+            Tab[i] = DELETED;
+            size--;
+        } else
+            System.out.println("There is not the element");
     }
-    public void Member( Object x ) {
-        
-    }
-    public void Insert( Object x ) {
-        
-    }
-    public void Delete( Object x ) {
-        
+    public boolean Element(Element e) {
+        int i = Hash(e.key);
+        int j = 0;
+        while( j <= DIMTAB && Tab[i] != VOID && Tab[i] != e ){
+            i = (i+1) % DIMTAB;
+            j++;
+        }
+        if( Tab[i] == e ){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
