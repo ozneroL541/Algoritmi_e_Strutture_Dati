49e3cf9dca26fd8201ca46d64ee01a965d2da7a2
ozneroL541 <l.21radice@gmail.com>
2023-07-26 11:23:33 +0200

    comments

--- src/Data_Structures/Hash/Element.java
+++ src/Data_Structures/Hash/Element.java
@@ -1,10 +1,19 @@
 package Data_Structures.Hash;
 
 public class Element {
-    String key;
-    String elem;
+    // Field key
+    public String key;
+    // Field element
+    public String elem;
+    /**
+     * Element Constructure
+     * @param k String
+     * @param v String
+     */
     public Element( String k, String v ){
+        // Set key
         this.key = k;
+        // Set element
         this.elem = v;
     }
 }
